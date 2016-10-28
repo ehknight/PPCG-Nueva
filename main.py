778d9ef6f070bf0918606db89cd57f1b84afbdab
hyperdo <super.ethan@gmail.com>
2016-10-27 18:31:01 -0700

    continuing work on ppcg

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from tinydb import TinyDB, Query
 from ast import literal_eval
 from flask.ext.stormpath import StormpathManager,login_required, user
 
-from uploads.PD.contest import go as contestMain
+from uploads.PD.contest import go as PD_contestMain
+from uploads.TTT.contest import go as TTT_contestMain
 from random import randrange
 
 app = Flask(__name__)
@@ -18,7 +19,9 @@ app.config['STORMPATH_ENABLE_MIDDLE_NAME'] = False
 app.config['STORMPATH_ENABLE_FORGOT_PASSWORD'] = True
 stormpath_manager = StormpathManager(app)
 
-app.config['UPLOAD_FOLDER'] = 'uploads/PD'
+challengeAcronym = "TTT"
+
+app.config['UPLOAD_FOLDER'] = 'uploads/'+challengeAcronym
 app.config['ALLOWED_EXTENSIONS'] = set(['py','js'])
 
 teamsScores=[]
@@ -31,22 +34,36 @@ Challenge = Query()
 
 currentChallengeName="""Meta Tic-Tac-Toe"""
 
-currentRules="""You must write a program to play the exciting game of meta tic-tac-toe! There are a couple of variations out there, but here is how we play it: We have a standard tic-tac-toe board but in each square there is another tic-tac-toe board. The turn flow works something like this:
+currentRules="""You must write a program to play the exciting game of meta tic-tac-toe!
+
+There are a couple of variations out there, but here is how we play it: We have a standard tic-tac-toe board but in each square there is another tic-tac-toe board. The turn flow works something like this:
 <ol>
 <li>If it is the first turn, Player 1 places an "O" in one of the squares in the center board. If not, Player 1 places an "O" in the board that coresponds to the square that Player 2 played in.</li>
 <li>Player 2 then goes in the board corresponding to which square Player 1 played in. For example, if Player 1 goes in the top right square, Player 2 must play in the top right board.</li>
-<li> Rinse and repeat until someone wins three boards in a row, similar to tic-tac-toe.</li>
+<li> Rinse and repeat until someone wins three meta-boards in a row, column, or diagonal, similar to tic-tac-toe.</li>
 </ol>
-If a board is filled up, then you are directed to a random, non-filled up board. If that made no sense, visit <a href="https://s3.amazonaws.com/mpacampcashchallenge/UltimateTicTacToe.pdf">this</a> link to get a sense of what's going on, then read the rules again because there are some changes."""
+
+If a board is filled up, then you get to pick which board you want to play on! (See next section for details.) If you send yourself to a filled up board, a random not-filled up board will be given to you.
+
+Because basic meta-tic-tac-toe is solved, we're adding a twist! Two people can win a board, which count towards winning the big meta-board for both of them.
+
+If that made no sense, visit <a href="https://s3.amazonaws.com/mpacampcashchallenge/UltimateTicTacToe.pdf">this</a> link to get a sense of what's going on, then read the rules again because there are some changes."""
 
 currentTask="""
-You must write a program meeting the requirements in the overarching
-competition rules that plays meta tic-tac-toe. Your function
-must take in parameters <tt>(teamNum, curState, activeBoardNum, wonBoards)</tt>, and be titled <tt>main</tt>. <tt>teamNum</tt> is either 1 or 0 and corresponds to "O" or "X". curState is the current state of the board, being a list of rows in a list of columns in a list of board rows in a list of board columns. So, if a game with a center board which has a 0 in the top-right corner of the center board were a curState, it would look like: <tt>[[[['','',''],['','',''],['','','']],[['','',''],['','',''],['','','']],[['','',''],['','',''],['','','']]],[[['','',''],['','',''],['','','']],[['','','0'],['','',''],['','','']],[['','',''],['','',''],['','','']]],[[[    '','',''],['','',''],['','','']],[['','',''],['','',''],['','','']],[['','',''],['',''    ,''],['','','']]]]</tt>. You could access that element by doing curState[1][1][0][2] (in English this translates to give me the second row of the board, then give me the second board of that, then give me the first row of the board, then give me the third column of that). activeBoardNum is a list of length 2. For example, if you were playing in the top-middle square, it activeBoardNum would be [0,1]. wonBoards is a 3x3 array of boards won. For example, if Player 0 has won the top left board it would look like <tt>[['0','',''],['','',''],['','','']]</tt>. Your function must return a list of length two of where you want to put your piece. For instance, if you want to go in the top right corner, you'd return <tt>[0,2]</tt>. Good luck and have fun!"""
+You must write a program meeting the requirements in the overarching competition rules that plays meta tic-tac-toe, consiting of two functions:
 
+Your function <tt>main</tt> must take in parameters <tt>(team, board, currentBoard, metaBoard)</tt>. <tt>teamNum</tt> is either 1 or 0 and corresponds to "O" or "X". <tt>currentState</tt> is the current state of the board, being a list of rows in a list of columns in a list of board rows in a list of board columns.
+
+So, if a game with a center board which has a 0 in the top-right corner of the center board were a curState, it would look like: <tt>[[[['','',''],['','',''],['','','']],[['','',''],['','',''],['','','']],[['','',''],['','',''],['','','']]],[[['','',''],['','',''],['','','']],[['','','0'],['','',''],['','','']],[['','',''],['','',''],['','','']]],[[['','',''],['','',''],['','','']],[['','',''],['','',''],['','','']],[['','',''],['','',''],['','','']]]]</tt>. You can put that into Python or JavaScript and moneky around with that, if you like. 
+
+You could access that top-right "0" element by doing curState[1][1][0][2]. (In English this translates to "Give me the second row of the board, then give me the second board of that, then give me the first row of the board, then give me the third column of that".) <tt>currentBoard</tt> is a list of length 2. For example, if you were playing in the top-middle square, it activeBoardNum would be [0,1]. <tt>metaBoard</tt> is a 3x3 array of boards won. For example, if Player 0 has won the top left board it would look like <tt>[['0','',''],['','',''],['','','']]</tt>. Your function must return a list of length two of where you want to put your piece. For instance, if you want to go in the top right corner, your <tt>main</tt> function would return <tt>[0,2]</tt>.
+
+You'll also write a function <tt>pick</tt> which picks a board for you to play on, should your opponent send you to a filled up square. Your function <tt>pick</tt> must take in parameters <tt>(team, board, metaBoard)</tt> and return a list of length 2. For instance, if you wanted to play on the middle-middle board, your pick function would return <tt>[1,1]</tt>.
+
+Thanks for taking part in PPCG. Good luck and have fun!"""
 
 @app.route('/')
-def ayy():
+def initialHomehtml():
     return render_template('index.html')
 
 @app.route('/index.html')
@@ -128,7 +145,7 @@ def donateUpload():
 
 @app.route('/run')
 def run():
-    print 'running judge program'
+    print('running judge program')
     global teamsScores
     global ignoreList
     totalReturn=contestMain(20)
@@ -150,7 +167,7 @@ def accounthtml():
     try:
         tempPN = user.custom_data['programName']
     except KeyError:
-        filesExt = [x for x in os.listdir(os.curdir+'/uploads/PD') if x!='contest.py'
+        filesExt = [x for x in os.listdir(os.curdir+'/uploads/'+challengeAcronym) if x!='contest.py'
                  and x[-3:]!='pyc' and x!='__init__.py' and x!='.DS_Store'
                  and x[0]!='.']
         files = [x[:-3] for x in filesExt]
---
+++ uploads/TTT/contest.py
@@ -0,0 +1,333 @@
+import random
+from itertools import chain
+import os, sys
+import execjs
+import signal
+
+def testEntry(teamNum, curState, activeBoardNum, wonBoards):
+    return False #ignore this
+
+entries = {'entry1': testEntry} #ignore this
+
+
+def printBoard(board):
+    f = [['meme']]
+    for bigRow in board:
+        for bigColumn in bigRow:
+            for row in bigColumn:
+                tempRow = []
+                for val in row[::1]:
+                    if val == None:
+                        tempRow.append(' ')
+                    else:
+                        tempRow.append(str(val))
+                f.append(tempRow)
+    for rowNum in range(28):
+        if rowNum == 0:
+            continue
+        for valNum in range(3):
+            if f[rowNum][valNum] == None:
+                f[rowNum][valNum] = ' '
+            f[rowNum][valNum] = str(f[rowNum][valNum])
+    print([f[1], f[4], f[7]])
+    print([f[2], f[5], f[8]])
+    print([f[3], f[6], f[9]])
+    print(['-----------------------------------------------'])
+    print([f[10], f[13], f[16]])
+    print([f[11], f[14], f[17]])
+    print([f[12], f[15], f[18]])
+    print(['-----------------------------------------------'])
+    print([f[19], f[22], f[25]])
+    print([f[20], f[23], f[26]])
+    print([f[21], f[24], f[27]])
+
+def won(*slots):
+    """
+    Checks if the slots are equal. 2 corresponds to both 0 and 1.
+    """
+    not2 = [x for x in slots if x!=2]
+    notNoneLen = len([x for x in slots if x!=None])
+    if notNoneLen != 3:
+        return None
+    if all(not2):
+        return 1
+    elif not any(not2):
+        return 0
+    else:
+        return None
+
+def checkWin(board):
+    # first check rows
+    wonList = []
+    for row in board:
+        wonList.append(won(*row))
+    # now checks cols
+    for col in zip(*board):
+        wonList.append(won(*col))
+    # check diags
+    wonList.append(won(board[0][0], board[1][1], board[2][2]))
+    wonList.append(won(board[0][0], board[1][1], board[2][2]))
+    wonList.append(won(board[0][2], board[1][1], board[2][0]))
+    if 1 in wonList and 0 in wonList:
+        return 2
+    notNone = [item for item in wonList if item != None]   
+    if len(notNone)==0: return None
+    assert all(e == notNone[0] for e in notNone) #check if all elems are equal
+    return notNone[0]
+
+def avaliableSpots(board, metaBoard):
+    availSpots = [] #list of avaliable spots in meta-board
+    for rowNum in range(len(metaBoard)):
+        row = metaBoard[rowNum]
+        for colNum in range(len(row)):
+            if None in chain(*board[rowNum][colNum]):
+                availSpots.append([rowNum, colNum])
+    return availSpots
+
+def playMove(funcs, teamNum, board, curBoard, metaBoard, debug=False):
+    # funcs should be {0:[mainFunc, pickFunc]...}
+    teamFunc = funcs[teamNum][0]
+    otherTeamPick = funcs[abs(teamNum-1)][1] 
+    try:
+        move1 = teamFunc(teamNum, board, curBoard, metaBoard) # not move1
+        if board[curBoard[0]][curBoard[1]][move1[0]][move1[1]] != None:
+            raise AssertionError #trying to move in a taken square
+        else:
+            board[curBoard[0]][curBoard[1]][move1[0]][move1[1]] = teamNum
+        for boardRow in range(3):
+            for boardCol in range(3):
+                won = checkWin(board[boardRow][boardCol])
+                if won != None: #check to see if won small board
+                    metaBoard[boardRow][boardCol] = won
+        bigWon = checkWin(metaBoard) #check to see if won meta board
+        if bigWon != None:
+            return ["BigWin", bigWon, board, metaBoard]
+            
+        availSpots = avaliableSpots(board, metaBoard)
+        if len(availSpots) == 0:
+            return ["Draw", board, curBoard, metaBoard]
+        elif move1 not in availSpots:
+            curBoard = otherTeamPick(1, board, metaBoard)
+            if curBoard not in availSpots:
+                if debug:
+                    print("Random Choice!!")
+                curBoard = random.choice(availSpots)
+        else:
+            curBoard = move1
+            
+    except AssertionError:
+        if debug:
+            print("Invalid move; skipping turn.")
+        pass
+    return ["NoWin", board, curBoard, metaBoard]
+
+def printTuple(tup):
+    return "["+str(tup[0])+", "+str(tup[1])+"]"
+
+def determineDrawWinner(metaBoard):
+    ones = 0
+    zeros = 0
+    for row in metaBoard:
+        for val in row:
+            if val == 1:
+                ones = ones + 1
+            if val == 0:
+                zeros = zeros + 1
+    if ones > zeros:
+        return 1
+    if zeros > ones:
+        return 0
+    return None
+
+def generateBlankBoard():
+    return [[[[None, None, None] for _ in range(3)] for _ in range(3)]
+             for _ in range(3)]
+
+def generateMetaBoard():
+    return [[None for _ in range(3)] for _ in range(3)]
+
+def playMatch(rounds, team1, team1pick, team2, team2pick, debug=True):
+    board = generateBlankBoard()
+    printBoard(board)
+    metaBoard = generateMetaBoard()
+    curBoard = [1, 1]
+    funcs = {0:[team1, team1pick], 1:[team2, team2pick]}
+    for turn in range(180):
+        if debug:
+            print("Current Board: "+printTuple(curBoard))
+        winCode, board, curBoard, metaBoard = \
+            playMove(funcs, 0, board, curBoard, metaBoard)
+        if winCode == "BigWin":
+            return board  # actually returning the winner
+        elif winCode == "Draw":
+            break
+        if debug:
+            printBoard(board)
+            print("Current Board: "+printTuple(curBoard))
+        winCode, board, curBoard, metaBoard = \
+            playMove(funcs, 1, board, curBoard, metaBoard)
+        if winCode == "BigWin":
+            return board  # actually returning the winner
+        elif winCode == "Draw":
+            break        
+        if debug:
+            print("Round complete, here's the board:")
+            printBoard(board)
+            print(metaBoard[0])
+            print(metaBoard[1])
+            print(metaBoard[2])
+    return None
+
+def testMatch():
+    def testFunc1(teamNum, board, curBoard, metaBoard):
+        return eval(input("TEAM 0: What's your move, team " + str(teamNum) + "? "))
+        
+    def testFuncPick1(teamNum, board, metaBoard):
+        return eval(input("TEAM 0: The opponent has picked a filled up square. Please "+
+                     "pick a square that you'd like to play on."))
+                     
+    def testFunc2(teamNum, board, curBoard, metaBoard):
+        return eval(input("TEAM 1: What's your move, team " + str(teamNum) + "? "))
+    
+    def testFuncPick2(teamNum, board, metaBoard):
+        return eval(input("TEAM 1: The opponent has picked a filled up square. Please "+
+                     "pick a square that you'd like to play on."))
+    
+    playMatch(testFunc1, testFuncPick1, testFunc2, testFuncPick2)
+
+def playGame(rounds, team1, team1pick, team2, team2pick):
+    teamTotals = [0, 0]
+    for _ in range(rounds):
+        match = playMatch(rounds, team1, team1pick, team2, team2pick)
+        if match == None:
+            continue
+        teamTotals[match] += 1
+    return teamTotals
+
+def go(rounds, debug=False):
+    class TimeoutException(Exception):   # Custom exception class
+        pass
+    
+    class InitializationError(Exception):
+        pass
+    
+    class PassThrough(Exception):
+        pass
+    
+    def timeout_handler(signum, frame):   # Custom signal handler
+        raise TimeoutException
+
+    TeamList=[x[0:-3] for x in os.listdir(os.curdir+'/uploads/TTT') if x!='contest.py'
+              and x[-3:]!='pyc' and x!='__init__.py' and x!='.DS_Store'
+              and x[0]!='.']
+    if debug:
+        print(TeamList)
+    TeamFunctions={}
+    combinedArray={}
+    defaultArgs = [0, generateBlankBoard(), [1,1], generateMetaBoard()]
+    
+    sys.path.insert(0, './uploads/TTT')
+    for i in range(len(TeamList)):
+        combinedArray[TeamList[i]]=0
+    signal.signal(signal.SIGALRM, timeout_handler)
+    toDelete=[]
+    for teamNum in range(len(TeamList)):
+        signal.alarm(0)
+        try:
+            new_module = __import__(TeamList[teamNum])
+            t = str(TeamList[teamNum])
+            print('imported '+str(TeamList[teamNum]))
+            signal.alarm(1)
+            try:
+                a = new_module.main(*defaultArgs)
+            except:
+                print('Error code 1')
+                print('Signal timed out')
+                raise InitializationError
+            finally:
+                signal.alarm(0)
+            try:
+                if len(a)!=2:
+                    raise InitializationError
+            except:
+                print('Error code 2')
+                print('Function returned unexpected value')
+                print('Function returned '+str(a))
+                raise InitializationError
+            else:
+                TeamFunctions[t] = (new_module.main, new_module.pick)
+        except ImportError:
+            try:
+                t = str(''.join(list(TeamList[teamNum])[::1]))
+                jfunc=(open('./uploads/TTT/'+t+'.js','r')).read()
+                ctx = execjs.compile(jfunc)
+                trans = lambda *args: ctx.call("main", *args)
+                transPick = lambda *args: ctx.call("pick", *args)
+                try:
+                    signal.alarm(2)
+                    try:
+                        a = trans(*defaultArgs)
+                    except:
+                        print('Error code 3')
+                        print('Signal timed out')
+                        raise InitializationError
+                    try:
+                        if len(a)!=2:
+                            raise InitializationError
+                    except:
+                        print('Error code 4')
+                        print('Function returned unexpected value')
+                        print('Function returned '+str(a))
+                        raise InitializationError
+                    else:
+                        raise PassThrough
+                except PassThrough:
+                    TeamFunctions[str(t)] = (trans, transPick)
+                    continue
+                finally:
+                    signal.alarm(0)
+                    del trans
+                    del transPick
+            except InitializationError:
+                toDelete.append(TeamList[teamNum])
+        except:
+            print('Error code 6')
+            print('WTF')
+            print('ignoring '+TeamList[teamNum])
+            toDelete.append(TeamList[teamNum])
+    for i in toDelete:
+        TeamList.remove(i)
+    scoreArray=[0 for x in range(len(TeamList))]
+    for foo in TeamList:
+        team1, team1pick = TeamFunctions[foo]
+        for bar in TeamList:
+            team2, team2pick = TeamFunctions[bar]
+            if (foo == bar):
+                break
+            if debug:
+                print("A match will now begin between "+foo+" and "+bar)
+            matchScore = playMatch(rounds, team1, team1pick, team2, team2pick)
+            if debug:
+                print(matchScore)
+            scoreArray[TeamList.index(foo)] += matchScore[1]
+            scoreArray[TeamList.index(bar)] += matchScore[0]
+    if debug:
+        print('-----------------------Game-Over-----------------------')
+        print('After ' + str(rounds) + " rounds, the scores are:")
+    bestScore = 0
+    bestScoreInd = 9999
+    for p in range(len(TeamList)):
+        if debug:
+            print(TeamList[p] + " : " + str(scoreArray[p]))
+        if (bestScore < scoreArray[p]):
+            bestScore = scoreArray[p]
+            #print(bestScore)
+            bestScoreInd = p
+            #print(bestScoreInd)
+    print('The winner was: ' + TeamList[bestScoreInd])
+    completeScore = zip(TeamList, scoreArray)
+    completeScore = sorted(completeScore, key = lambda x: x[1], reverse = True)
+    return (completeScore, toDelete)
+
+if __name__=="__main__":
+    go()
